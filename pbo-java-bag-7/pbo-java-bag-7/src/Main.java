afb31867863a3871de43323f05fd4a52fa0cd014
Evelin Salsabila A <108512588+EvelinSA05@users.noreply.github.com>
2023-08-22 03:50:18 +0700

    pbo java bagian 7

---
+++ pbo-java-bag-7/pbo-java-bag-7/src/Main.java
@@ -0,0 +1,51 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Kalkulator hitung = new Kalkulator(5, 4);
+
+        // hitung.tampil();
+
+        int a, b, c;
+        a = 15;
+        b = 14;
+        c = 7;
+
+        boolean hasil;
+
+        hasil = a != b;
+
+        System.out.println(hasil);
+
+        int d = 5, e = 8, f = 9;
+
+        double pi = 3.14;
+        double pecahan = 5.0;
+
+        byte angka = 20;
+
+        char x = 'g';
+
+        c = ++a;
+    }
+}
+
+class Kalkulator{
+   Kalkulator(int a, int b){
+       System.out.println("Belajar Konstruktor " + a + b);
+   }
+
+   void tampil(){
+       byte a = 127;
+
+       char b = 'B';
+
+       short s = 32767;
+
+       float f = 112345;
+
+       double d = 1.5;
+
+       System.out.println("\" hello world \"");
+   }
+}
